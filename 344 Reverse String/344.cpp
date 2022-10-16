2328d68329754bf72f39df4f19dd47e34b4adc83
Bosh-Kuo <evaktm1680@gmail.com>
2022-10-16 16:09:19 +0800

    344

---
+++ 344 Reverse String/344.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+/*
+time complexity: O(n)
+tag: two pointer
+note: 以左右兩端為起點一邊互換一邊向中間走，最多走 n/2 步可完成
+*/
+
+class Solution
+{
+public:
+    void reverseString(vector<char> &s)
+    {
+        int left = 0, right = s.size() - 1;
+        recursive_swap(s, left, right);
+    }
+    void recursive_swap(vector<char> &s, int left, int right)
+    {
+        if (left >= right)
+            return;
+        swap(s[left], s[right]);
+        recursive_swap(s, left+1, right-1);
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector<char> test_case = {'h', 'e', 'l', 'l', 'o'};
+    sol.reverseString(test_case);
+    for (char i : test_case)
+    {
+        cout << i << " " << endl;
+    }
+    return 0;
+}
