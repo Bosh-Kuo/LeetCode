80b505845c6557dd516045d40f0f14d9fff43bd0
Bosh-Kuo <evaktm1680@gmail.com>
2023-10-16 23:38:54 +0800

    2722, 2724

---
+++ solutions/2722 Join Two Arrays by ID/2722.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @return {Array}
+ */
+var join = function (arr1, arr2) {
+  const result = {};
+  arr1.forEach((item) => {
+    result[item.id] = item;
+  });
+
+  arr2.forEach((item) => {
+    if (item.id in result) result[item.id] = { ...result[item.id], ...item };
+    else result[item.id] = item;
+  });
+  return Object.values(result);
+};
---
+++ solutions/2724 Sort By/2724.js
@@ -0,0 +1,15 @@
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+var sortBy = function (arr, fn) {
+  function compareFn(a, b) {
+    if (fn(a) < fn(b)) {
+      return -1;
+    } else if (fn(a) > fn(b)) {
+      return 1;
+    } else return 0;
+  }
+  return arr.sort(compareFn);
+};
