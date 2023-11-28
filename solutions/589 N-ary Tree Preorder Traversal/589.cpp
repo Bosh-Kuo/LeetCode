1284087d3215cc41860c9a67ddc3a9f25def239b
Bosh-Kuo <evaktm1680@gmail.com>
2023-11-29 01:03:29 +0800

    589

---
+++ solutions/589 N-ary Tree Preorder Traversal/589.cpp
@@ -0,0 +1,26 @@
+/*
+time complexity: O(n)
+tags: Stack, Tree, Depth-First Search
+note: pre-order traversal 為 DFS，在 recursion function 中先執行本身，在逐個執行 children node
+*/
+class Solution
+{
+public:
+    void preOrderTraversal(Node *root, vector<int> &answer)
+    {
+        if (root == nullptr)
+            return;
+        answer.push_back(root->val);
+        int numOfChildren = root->children.size();
+        for (int i = 0; i < numOfChildren; i++)
+        {
+            preOrderTraversal(root->children[i], answer);
+        }
+    }
+    vector<int> preorder(Node *root)
+    {
+        vector<int> answer;
+        preOrderTraversal(root, answer);
+        return answer;
+    }
+};
