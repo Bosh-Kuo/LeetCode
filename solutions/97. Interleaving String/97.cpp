58a5c75e3a5b500bf183f9f602cb77b61711b89e
Bosh-Kuo <boshkuo.dev@gmail.com>
2024-07-16 00:13:16 +0800

    97

--- solutions/97. Interleaving String/97.cpp
+++ solutions/97. Interleaving String/97.cpp
@@ -1,7 +1,7 @@
 /*
 time complexity: O(m*n)
 tags: String, Dynamic Programming
-note:
+note: 同註解
 */
 
 class Solution
@@ -17,25 +17,32 @@ public:
         if (m < n)
             return isInterleave(s2, s1, s3);
 
-        // dp[j] 表示 s3 前(i+j)個字元組成的 substring 可以由 s1 的前 i 個字元與 s2 的前 j 個字元組成
-        // 當 s1[i-1] == s3[i+j-1]，表示 s3 前(i+j-1)個字元組成的 substring 可能由 s1 的前 i-1 個字元與 s2 的前 j 個字元組成 => dp[j]
-        // 當 s2[j-1] == s3[i+j-1]，表示 s3 前(i+j-1)個字元組成的 substring 可能由 s1 的前 i 個字元與 s2 的前 j-1 個字元組成 => dp[j-1]
-        vector<bool> dp(n + 1, false);
-        dp[0] = true;
+        // dp[i][j] 表示 s3 前(i+j)個字元組成的 substring 可以由 s1 的前 i 個字元與 s2 的前 j 個字元組成
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+        dp[0][0] = true;
 
-        for (int j = 1; j <= n; ++j)
+        // 假設 s2 貢獻 0 個字元
+        for (int i = 1; i <= m; i++)
         {
-            dp[j] = dp[j - 1] && s2[j - 1] == s3[j - 1];
+            dp[i][0] = dp[i - 1][0] && s1[i - 1] == s3[i - 1];
         }
 
-        for (int i = 1; i <= m; ++i)
+        // 假設 s1 貢獻 0 個字元
+        for (int j = 1; j <= n; j++)
         {
-            dp[0] = dp[0] && s1[i - 1] == s3[i - 1];
-            for (int j = 1; j <= n; ++j)
+            dp[0][j] = dp[0][j - 1] && s2[j - 1] == s3[j - 1];
+        }
+
+        // 當 s1[i-1] == s3[i+j-1]，表示 s3 前(i+j-1)個字元組成的 substring 可能由 s1 的前 i-1 個字元與 s2 的前 j 個字元組成 => dp[i-1][j]
+        // 當 s2[j-1] == s3[i+j-1]，表示 s3 前(i+j-1)個字元組成的 substring 可能由 s1 的前 i 個字元與 s2 的前 j-1 個字元組成 => dp[i][j-1]
+        for (int i = 1; i <= m; i++)
+        {
+            for (int j = 1; j <= n; j++)
             {
-                dp[j] = (dp[j] && s1[i - 1] == s3[i + j - 1]) || (dp[j - 1] && s2[j - 1] == s3[i + j - 1]);
+                dp[i][j] = (dp[i - 1][j] && s1[i - 1] == s3[i + j - 1]) || (dp[i][j - 1] && s2[j - 1] == s3[i + j - 1]);
             }
         }
-        return dp[n];
+
+        return dp[m][n];
     }
 };
